602b99ffde33d959de154f1e6dcf986c179942e9
atu <61017515+3flower@users.noreply.github.com>
2020-11-06 15:29:08 +0900

    [add]task_model

---
+++ db/migrate/20201106062537_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+
+      t.integer :user_id
+      t.string  :title
+      t.string  :content
+      # 担当者と添付ファイル追加
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_060724) do
+ActiveRecord::Schema.define(version: 2020_11_06_062537) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
