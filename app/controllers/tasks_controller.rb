c512c05ddbe1e20f86d6a0053247612dd96d6843
atu <61017515+3flower@users.noreply.github.com>
2020-11-08 16:57:27 +0900

    [add]task_index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,14 +6,33 @@ class TasksController < ApplicationController
     # edit
 
     # index
+    # binding.pry
+    @tasks = Task.all
+    @tasks_not = Task.where(progress: "未着手")
+    @tasks_commencement = Task.where(progress: "着手中")
+    @tasks_completion = Task.where(progress: "完了")
   end
 
   def create
+
+    @task = current_user.tasks.new(tasks_params)
+    if @task.save
+      # flash[:notice] = "投稿しました"
+      redirect_to root_path
+    else
+      render :top
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :content, :progress)
   end
 end
