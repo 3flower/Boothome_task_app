d66561a095cfabfedc43aeefd8f09d2e0d18cbcf
atu <61017515+3flower@users.noreply.github.com>
2020-11-16 18:55:36 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,14 +74,22 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :domain => 'herokuapp.com',
+  # config.action_mailer.smtp_settings = {
+  #   :enable_starttls_auto => true,
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => 587,
+  #   :domain => 'herokuapp.com',
+  #   :authentication => :plain,
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_PASSWORD'],
+  # }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
